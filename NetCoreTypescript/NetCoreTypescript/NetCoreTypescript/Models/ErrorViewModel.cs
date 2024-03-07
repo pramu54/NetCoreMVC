884099998f6f5e462db9c996e128dea75ecf8bc4
pramu54 <mohadipramudiono@gmail.com>
2024-03-07 13:53:23 +0700

    Initial Commit

---
+++ NetCoreTypescript/NetCoreTypescript/NetCoreTypescript/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreTypescript.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
